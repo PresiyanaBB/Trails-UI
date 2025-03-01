2d980a624348806612a21c35f81eeaf335934f3c
Presiyana Borisova <presiana03@gmail.com>
2025-03-01 19:06:10 +0200

    Header styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListArtistComponent from './components/ListArtistComponent'
 
 function App() {
 
   return (
     <>
+      <HeaderComponent />
       <ListArtistComponent />
+      <FooterComponent />
 
     </>
   )
--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,14 +1,36 @@
 import React from "react";
+import '../styles/header.css';
 
 const HeaderComponent = () => {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-            <div>
-                <a href="/" className="navbar-brand">
-                    Artist Management System
-                </a>
-            </div>
-        </nav>
+        <header>
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="index.html">
+                        <img src="/step.png" alt="home" id="logo-to-home-page" className="animated-logo" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Начало</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/artists">Артисти</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/gallery">Галерия</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/contacts">Контакти</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header >
     );
 }
 
